0223a88bee7c3c3ca8e536af963933302d433f5e
moneymakeme <130539471+moneymakeme@users.noreply.github.com>
2024-09-16 19:53:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    var message = document.getElementById('message');
+    var images = document.getElementById('images');
+    message.style.display = 'block';
+    images.style.display = 'flex';
+}
